af157d3a860d48ed4a571f07cea2307aa8954fb1
acatzk <joshuaimalay@gmail.com+>
2020-10-07 21:24:48 +0800

    Import the VueCompositionalAPI in global

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import BackToTop from 'vue-backtotop';
+import VueCompositionAPI from '@vue/composition-api'
+
+Vue.use(VueCompositionAPI)
 
 Vue.config.productionTip = false;
 Vue.use(BackToTop);
