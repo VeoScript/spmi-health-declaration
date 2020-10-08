3ded0616e09909460a6189d101f49dbadec29985
acatzk <joshuaimalay@gmail.com+>
2020-10-08 11:43:15 +0800

    Design simple Welcome root page

---
+++ src/views/Welcome.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <v-container class="fill-height" fluid>
+      <v-row align="center" justify="center">
+        <v-card flat>
+          <v-card-text>
+            <v-img src='@/assets/spmi_official.png' max-width="450"/>
+            <h2>Covid-19 Health Declaration</h2>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn 
+              x-large 
+              block 
+              color="blue darken-2" 
+              dark to="/login"
+              tile
+              depressed
+            >
+              Login Here!
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-row>
+    </v-container>
+  </v-app>
+</template>
