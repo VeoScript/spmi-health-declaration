98edc66bbe07219ba4af6e1186f4ab51f3ada479
acatzk <joshuaimalay@gmail.com+>
2020-10-08 08:17:26 +0800

    Import SideBar vue component and passed props visible

--- src/components/layouts/NavBar.vue
+++ src/components/layouts/NavBar.vue
@@ -46,109 +46,26 @@
   </v-app-bar>
 
   <!-- SIDEBAR VIEW COMPONENT -->
-  <v-navigation-drawer app v-model="drawer">
-    <v-list-item>
-      <v-list-item-content>
-        <v-list-item-title class="title">
-          <v-img src='@/assets/spmi_official.png' max-width="120"/>
-        </v-list-item-title>
-        <v-list-item-subtitle>
-          COVID-19 HEALTH DECLARATION
-        </v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item>
-
-    <v-divider></v-divider>
-
-    <v-list
-      dense
-      nav
-    >
-    
-      <router-link to="/">
-        <v-list-item
-          link
-        >
-          <v-list-item-icon>
-            <v-icon>mdi-comment-check</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>Health Checklist</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </router-link>
-      
-      <router-link to="/">
-        <v-list-item
-          link
-        >
-          <v-list-item-icon>
-            <v-icon>mdi-file</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>Daily Records</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </router-link>
-
-      <router-link to="/">
-        <v-list-item
-          link
-        >
-          <v-list-item-icon>
-            <v-icon>mdi-account-box</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>Account</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </router-link>
-
-      <router-link to="/">
-        <v-list-item
-          link
-        >
-          <v-list-item-icon>
-            <v-icon>mdi-account-lock</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>Administrator</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </router-link>
-
-      <router-link to="/">
-        <v-list-item
-          link
-        >
-          <v-list-item-icon>
-            <v-icon>mdi-account-arrow-right</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>Sign Out</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </router-link>
-    </v-list>
-  </v-navigation-drawer>
+  <side-bar
+    :visible="drawer"
+    @close="drawer = false"
+  />
+  
   </div>
 </template>
 
 <script>
-export default {
-  name: 'NavBar',
-  components: {
-    //SideBar: () => import('@/components/SideBar'),
-  },
-  data: () => ({
-    drawer: null
-  })
-}
+  export default {
+    name: 'NavBar',
+    components: {
+      SideBar: () => import('./SideBar'),
+    },
+    data () {
+      return {
+        drawer: true
+      }
+    }
+  }
 </script>
 
 <style>
