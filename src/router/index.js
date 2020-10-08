bfd191d9fc31d0d5206780a349890820b49ffb61
acatzk <joshuaimalay@gmail.com+>
2020-10-08 11:31:30 +0800

    Added router guard for checking of user authenticated

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,20 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import { auth } from '@/services'
 
 Vue.use(VueRouter);
 
 const routes = [
   {
+    path: '/',
+    name: 'welcome',
+    component: () => import('@/views/Welcome')
+  },
+  {
     path: "/v",
     name: "home",
     component: () => import('@/views/Home'),
+    meta: { requiresAuth: true },
     children: [
       {
         path: 'health-checklist',
@@ -31,6 +38,19 @@ const routes = [
 const router = new VueRouter({
   routes,
   mode: 'history'
-});
+})
 
-export default router;
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+  const currentUser = auth.currentUser
+
+  if (requiresAuth && !currentUser) {
+    next('/login')
+  } else if (!requiresAuth && currentUser) {
+    next('/v/health-checklist')
+  } else {
+    next()
+  }
+})
+
+export default router
