6b8b5a3dac7ec59d33139cd526834655f0a38bc6
acatzk <joshuaimalay@gmail.com+>
2020-10-09 17:15:14 +0800

    Registration submit form in firebase and hasura successful

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -12,7 +12,6 @@
               <v-card-text>
                 <v-form
                   ref="form"
-                  @submit.prevent="onLogin"
                 >
                   <v-text-field
                     label="Email"
@@ -20,18 +19,18 @@
                     autocomplete="off"
                     filled
                     rounded
-                    v-model="state.form.email"
+                    v-model="form.email"
                     class="font-weight-light rounded-sm"
-                    :rules="[state.required('Email'), state.emailRules('Email')]"
+                    :rules="[required('Email'), emailRules('Email')]"
                   ></v-text-field>
                   <v-text-field
                     label="Password"
                     autocomplete="off"
                     filled
                     rounded
-                    v-model="state.form.password"
+                    v-model="form.password"
                     class="font-weight-light rounded-sm"
-                    :rules="[state.required('Password')]"
+                    :rules="[required('Password')]"
                   ></v-text-field>
                 </v-form>
               </v-card-text>
@@ -40,7 +39,7 @@
                   text 
                   color="primary"
                   class="text-capitalize"
-                  @click="state.dialog = true"
+                  @click="dialog = true"
                 > 
                   Create Account
                 </v-btn>
@@ -64,12 +63,14 @@
 
       <!-- Registration Dialog -->
       <register 
-        :form="state.form"
-        :visible="state.dialog"
-        @close="state.dialog = false"
+        :form="form"
+        :visible="dialog"
+        :error="error"
+        :loading="loading"
+        @close="dialog = false"
         @submitForm="onRegister"
         @clearForm="onClear"
-      />
+      ></register>
 
     </v-main>
   </v-app>
@@ -77,7 +78,9 @@
 
 <script>
 
-  import { reactive } from '@vue/composition-api'
+  import { auth } from '@/services'
+  import { toastAlertStatus } from '@/utils'
+  import { ADD_USERS_MUTATION } from '@/graphql/mutations'
 
   export default {
     name: 'login',
@@ -86,54 +89,122 @@
       Register: () => import('./Register')
     },
 
-    setup () {
-      let state = reactive({
+    data () {
+      return {
+        error: '',
         dialog: false,
+        loading: false,
         form: {
-          lastname: '',
-          firstname: '',
-          middlename: '',
-          age: '',
-          gender: '',
-          civilStatus: '',
-          contactNumber: '',
-          occupation: '',
-          department: '',
-          company: '',
-          email: '',
-          password: ''
+          lastname: 'villaruel',
+          firstname: 'jerome joseph',
+          middlename: 'robiato',
+          age: '21',
+          gender: 'male',
+          civilStatus: 'single',
+          contactNumber: '0952231231',
+          occupation: 'IT Coordinator',
+          department: 'Health',
+          company: 'SPMI',
+          email: 'jerome@gmail.com',
+          password: 'Ilusmdm123@'
         },
+        showPass: false,
         required (propertyType) { 
           return v => v && v.length > 0 || `${propertyType} is required.`
         },
         emailRules (propertyType) {
           return v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || `${propertyType} address must be valid.`
         }
-      })
-
-      onRegister = () => {
-        alert(state.form)
       }
+    },
 
-      onClear = () => {
-        this.$refs.form.reset()
-        state.form = {}
-      }
+    methods: {
+      
+      // ===== REGISTER USERS FIREBASE AND HASURA ====
+      onRegister () {
+        this.loading = true
+        const { email, password } = this.form
+        auth
+          .createUserWithEmailAndPassword(email, password)
+          .then((firebase) => {
+            this.saveInHasura (firebase)
+            toastAlertStatus('success', 'Successfully Registered.') 
+          })
+          .catch(error => {
+            this.errorProvider(error)
+          })
+      },
 
-      onLogin = () => {
+      // ====== SAVE INFORMATION IN HASURA ====
+      saveInHasura (firebase) {
+        const { lastname, firstname, middlename, age,
+          gender, civilStatus, contactNumber, occupation,
+          department, company, email, password } = this.form
+
+        this
+         .$apollo
+         .mutate({
+           mutation: ADD_USERS_MUTATION,
+           variables: {
+             firebase_id: firebase.uid,
+             lastname,
+             firstname,
+             middlename,
+             age,
+             gender,
+             civil_status: civilStatus,
+             contact_number: contactNumber,
+             occupation,
+             department,
+             company,
+             email,
+             password
+           }
+         })
+         .then(() => {
+            this.loading = false
+            this.$router.push('/v/health-checklist')
+            this.onClear() // CALLING CLEAR AFTER SUCCESSFULLY REGISTER
+         })
+         .catch(error => {
+            this.loading = false
+            this.errorProvider(error)
+          })
+      },
+
+      // ===== CLEAR TEXTBOX AND RESET VALIDATION FORM ====
+      onClear () {
+        let self = this
+        self.$refs.form.reset()
+        self.form = {}
+      },
+
+      // ==== LOGIN AUTHENTICATION ==== 
+      onLogin () {
         let self = this
         if (self.$refs.form.validate()) {
           alert('GOOOD')
         }
-      }
+      },
 
-      return {
-        state,
-        onRegister,
-        onClear,
-        onLogin
+      // ==== ERROR PROVIDER TEXT MESSAGE ====
+      errorProvider (error) {
+        this.loading = false
+        let errorCode = error.code;
+        let errorMessage = error.message;
+        if (errorCode) {
+          toastAlertStatus('error', errorCode.split('/')[1])
+          return this.error = errorCode
+        } else if (errorMessage) {
+          toastAlertStatus('error', errorMessage.split('/')[1])
+          return this.error = errorMessage
+        } else {
+          toastAlertStatus('error', error.split('/')[1])
+          return this.error = error
+        }
       }
     }
+
 
   }
 </script>
