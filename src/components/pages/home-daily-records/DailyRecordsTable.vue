ec5c79c386f0502d1862aa57318cd1ce2c80817f
acatzk <joshuaimalay@gmail.com+>
2020-10-10 21:57:04 +0800

    Create DailyRecordsTable vue compnent

---
+++ src/components/pages/home-daily-records/DailyRecordsTable.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-card flat>
+    <v-card-title>
+      Health Declaration
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-card-subtitle>Daily Records</v-card-subtitle>
+    <v-data-table
+      :headers="headers"
+      :items="dailyrecord"
+      :search="search"
+    ></v-data-table>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        search: '',
+        headers: [
+          {
+            text: 'DATE',
+            align: 'start',
+            sortable: false,
+            value: 'date',
+          },
+          { text: 'SYMPTOMS', value: 'symptoms' },
+          { text: 'TRAVEL HISTORY', value: 'travelhistory' },
+          { text: 'PURPOSE(S)', value: 'purpose' },
+        ],
+        dailyrecord: [
+          {
+            date: '09-13-2020',
+            symptoms: 'None of the Above',
+            travelhistory: 'None of the Above',
+            purpose: 'DUTY',
+          },
+          {
+            date: '09-14-2020',
+            symptoms: 'Fever, Headache, Sore Throat, Cough',
+            travelhistory: 'Metro Manila,  Tacloban City',
+            purpose: 'VISIT',
+          },
+          {
+            date: '09-15-2020',
+            symptoms: 'Headache',
+            travelhistory: 'Ormoc City',
+            purpose: 'DUTY',
+          },
+        ],
+      }
+    },
+  }
+</script>
