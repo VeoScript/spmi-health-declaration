8a723029806607723f1f1c611e2d483b5d8be976
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2020-10-10 18:08:38 +0800

    added data table component

--- src/components/pages/home-daily-records/Index.vue
+++ src/components/pages/home-daily-records/Index.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>
-      <h1>Daily Records</h1>
+  <div class="dailyrecords">
+    <v-container class="mt-5" fluid>
+      <!-- daily records table view -->
+      <daily-records-table/>
+    </v-container>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'dailyrecords',
+  components: {
+    DailyRecordsTable: () => import('@/components/pages/home-daily-records/DailyRecordsTable')
+  }
+}
+</script>
