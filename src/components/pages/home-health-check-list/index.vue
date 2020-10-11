87b87cd0977519e821ff47385c4377ec2c9aa811
acatzk <joshuaimalay@gmail.com+>
2020-10-11 14:20:56 +0800

    Checkbox field added generic slot pass thru parent component

--- src/components/pages/home-health-check-list/index.vue
+++ src/components/pages/home-health-check-list/index.vue
@@ -17,7 +17,7 @@
               v-for="user in users" 
               :key="user.id" :user="user"
             >
-            </user-card>
+            </user-card> <!-- USER INFORMATION -->
 
           </v-col>
         </v-row>
@@ -25,24 +25,126 @@
           <v-col sm="6" md="6" cols="12">
 
             <!-- SYMPTOMS CARD -->
-            <symptoms-card
-
-            />
-          </v-col>
+            <symptoms-card>
+              <template #symptoms>
+                <v-row no-gutters>
+                  <v-col sm="6" md="6" cols="12">
+                    <v-checkbox
+                      label="Fever"
+                      hide-details
+                      value="Fever"
+                      v-model="checkList.symptoms"
+                      @change="onChangeSymptomatic"
+                    ></v-checkbox>
+                    <v-checkbox
+                      label="Headache"
+                      value="Headache"
+                      hide-details
+                      v-model="checkList.symptoms"
+                      @change="onChangeSymptomatic"
+                    ></v-checkbox>
+                    <v-checkbox
+                      label="Sore Throat"
+                      value="Sore Throat"
+                      hide-details
+                      v-model="checkList.symptoms"
+                      @change="onChangeSymptomatic"
+                    ></v-checkbox>
+                    <v-checkbox
+                      label="Body Weakness/Pain"
+                      value="Body Weakness/Pain"
+                      v-model="checkList.symptoms"
+                      @change="onChangeSymptomatic"
+                      hide-details
+                    ></v-checkbox>
+                  </v-col>
+                  <v-col sm="6" md="6" cols="12">
+                    <v-checkbox
+                      label="Cough"
+                      value="Cough"
+                      hide-details
+                      v-model="checkList.symptoms"
+                      @change="onChangeSymptomatic"
+                    ></v-checkbox>
+                    <v-checkbox
+                      label="Difficulty in Breathing"
+                      value="Difficulty in Breathing"
+                      hide-details
+                      v-model="checkList.symptoms"
+                      @change="onChangeSymptomatic"
+                    ></v-checkbox>
+                    <v-checkbox
+                      label="None of the Above"
+                      value="None of the Above"
+                      hide-details
+                      v-model="noSymptoms"
+                      @click="onChangeNoSymptoms"
+                    ></v-checkbox>
+                  </v-col>
+                </v-row>
+                <hr class="my-4"/>
+                <v-row no-gutters>
+                  <v-col sm="12" md="12" cols="12" >
+                    <span>
+                      Do you have a family members tested for
+                      covid-19 RT-PCR test?  
+                    </span>
+                    <div class="d-flex">
+                      <v-checkbox
+                        label="Yes"
+                        value="Yes"
+                        hide-details
+                        v-model="familyMemberTestedRTPCR"
+                      ></v-checkbox>
+                      <v-checkbox
+                        label="No"
+                        value="No"
+                        class="ml-4"
+                        hide-details
+                        v-model="familyMemberTestedRTPCR"
+                      ></v-checkbox>
+                    </div>
+                  </v-col>
+                </v-row>
+                <br>
+                <v-row no-gutters>
+                  <v-col sm="12" md="12" cols="12" >
+                    <span>
+                      Do you have a neighbors tested for
+                      covid-19 RT-PCR test?
+                    </span>
+                    <div class="d-flex">
+                      <v-checkbox
+                        label="Yes"
+                        value="Yes"
+                        hide-details
+                        v-model="neighborTestedRTPCR"
+                      ></v-checkbox>
+                      <v-checkbox
+                        label="No"
+                        value="No"
+                        class="ml-4"
+                        hide-details
+                        v-model="neighborTestedRTPCR"
+                      ></v-checkbox>
+                    </div>
+                  </v-col>
+                </v-row>
+              </template>
+            </symptoms-card>
+          </v-col> <!-- END SYMPTOMS CARD -->
 
           <v-col sm="6" md="6" cols="12">
             <!-- TRAVEL HISTORY CARD -->
-            <travel-history-card
-
-            />
+            <travel-history-card>
+              
+            </travel-history-card>
           </v-col>
-          
-
           
           <check-list-preview-dialog 
             :visible="dialog"
             @close-dialog="dialog = false"
-          />
+          ></check-list-preview-dialog> <!-- HEALTH CHECK PREVIEW -->
 
           <v-col sm="12" md="12" cols="12" class="mt-3 text-right">
             <v-btn
@@ -66,6 +168,7 @@
 <script>
   import gql from 'graphql-tag'
   import { auth } from '@/services'
+  import { GET_USER_BASIC_INFO } from '@/graphql/queries'
   export default {
     name: "HealthChecklist",
     components: {
@@ -76,23 +179,27 @@
     },
     data () {
       return {
-        dialog: false
+        dialog: false,
+        noSymptoms: '',
+        checkList: {
+          symptoms: [],
+          travelHistory: []
+        },
+        familyMemberTestedRTPCR: '',
+        neighborTestedRTPCR: ''
+      }
+    },
+    methods: {
+      onChangeNoSymptoms () {
+        this.checkList.symptoms = []
+      },
+      onChangeSymptomatic () {
+        this.noSymptoms = null
       }
     },
     apollo: {
       users: {
-        query: gql`
-          query userAccountTypeQuery($firebase_id: String) {
-            users(where: {firebase_id: {_eq: $firebase_id}}) {
-              id
-              lastname
-              firstname
-              middlename
-              company
-              occupation
-            }
-          }
-        `,
+        query: GET_USER_BASIC_INFO,
         variables () {
           return {
             firebase_id: auth ? auth.currentUser.uid : undefined
