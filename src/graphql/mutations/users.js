041fde593ba5eb3cc92c7b77266b86464bdbe73a
acatzk <joshuaimalay@gmail.com+>
2020-10-11 10:52:43 +0800

    Added field of nationality to insert data

--- src/graphql/mutations/users.js
+++ src/graphql/mutations/users.js
@@ -1,8 +1,8 @@
 import gql from 'graphql-tag'
 
 export const ADD_USERS_MUTATION = gql`
-  mutation addUserMutation($firebase_id: String!, $lastname: String, $firstname: String, $middlename: String, $age: String, $gender: String, $civil_status: String, $contact_number: String, $occupation: String, $department: String, $company: String, $email: String, $password: String) {
-    insert_users(objects: {firebase_id: $firebase_id, lastname: $lastname, firstname: $firstname, middlename: $middlename, age: $age, gender: $gender, civil_status: $civil_status, contact_number: $contact_number, occupation: $occupation, department: $department, company: $company, email: $email, password: $password}) {
+  mutation addUserMutation($firebase_id: String!, $lastname: String, $firstname: String, $middlename: String, $age: String, $gender: String, $civil_status: String, $contact_number: String, $occupation: String, $department: String, $company: String, $email: String, $password: String, $nationality: String) {
+    insert_users(objects: {firebase_id: $firebase_id, lastname: $lastname, firstname: $firstname, middlename: $middlename, age: $age, gender: $gender, civil_status: $civil_status, contact_number: $contact_number, occupation: $occupation, department: $department, company: $company, email: $email, password: $password, nationality: $nationality}) {
       affected_rows
       returning {
           id
