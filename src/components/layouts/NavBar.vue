571b5fc43b2aa46941337f71be86fcde3812037b
acatzk <joshuaimalay@gmail.com+>
2020-10-12 22:20:40 +0800

    Modified the style and added options array

--- src/components/layouts/NavBar.vue
+++ src/components/layouts/NavBar.vue
@@ -1,55 +1,48 @@
 <template>
-  <div class="navbar my-5">
+  <div>
     <!-- NAVBAR VIEW COMPONENT -->
-    <v-app-bar
-      class="navbartoolbar"
-      absolute
+    <v-app-bar 
+      app
+      flat
+      clipped-left
       color="white"
-      elevate-on-scroll
     >
-    <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-    
-    <v-toolbar-title>
-      <router-link to="/"><v-img src='@/assets/spmi_official.png' max-width="120"/></router-link>
-    </v-toolbar-title>
 
-    <v-spacer></v-spacer>
-    
-    <v-menu offset-y>
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn 
-          icon
-          v-bind="attrs"
-          v-on="on"
-        >
-          <v-icon>mdi-dots-vertical</v-icon>
-        </v-btn>
-      </template>
-      <v-list id="defaultcard">
-        <router-link to="/about">
-          <v-list-item link>
-            <v-list-item-title>About</v-list-item-title>
-          </v-list-item>
-        </router-link>
-        <router-link to="/">
-          <v-list-item link>
-            <v-list-item-title>Help</v-list-item-title>
-          </v-list-item>
-        </router-link>
-        <router-link to="/">
-          <v-list-item link>
-            <v-list-item-title>Terms & Conditions</v-list-item-title>
-          </v-list-item>
-        </router-link>
-      </v-list>
-    </v-menu>
-  </v-app-bar>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      
+      <v-toolbar-title>
+        <router-link to="/"><v-img src='@/assets/spmi_official.png' max-width="120"/></router-link>
+      </v-toolbar-title>
 
-  <!-- SIDEBAR VIEW COMPONENT -->
-  <side-bar
-    :visible="drawer"
-    @close="drawer = false"
-  />
+      <v-spacer></v-spacer>
+      
+      <v-menu offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn 
+            icon
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item 
+            v-for="(link, i) in options" :key="i"
+            link
+            :to="link.to"
+          >
+            <v-list-item-title>{{ link.text }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+
+    <!-- SIDEBAR VIEW COMPONENT -->
+    <side-bar
+      :visible="drawer"
+      @close="drawer = false"
+    ></side-bar>
   
   </div>
 </template>
@@ -62,7 +55,12 @@
     },
     data () {
       return {
-        drawer: true
+        drawer: true,
+        options: [
+          { text: 'About', to: '/v/about'},
+          { text: 'Help', to: '/v/help'},
+          { text: 'Terms and Condition', to: '/v/terms-condition'}
+        ]
       }
     }
   }
