984e0eae3104d96a21f88122e96f62d065f1de4a
acatzk <joshuaimalay@gmail.com+>
2020-10-12 22:23:39 +0800

    Displayed the selected symtoms and travel history

--- src/components/pages/home-health-check-list/CheckListPreviewDialog.vue
+++ src/components/pages/home-health-check-list/CheckListPreviewDialog.vue
@@ -6,7 +6,7 @@
     transition="dialog-bottom-transition"
     v-model="show"
   >
-    <v-card style="background: #fff;">
+    <v-card class="gray">
       <v-toolbar class="dgbox">
         <v-btn
           icon
@@ -20,80 +20,114 @@
           <v-btn
             text
           >
-            Save
+           <v-icon left>mdi-content-save</v-icon> Save
           </v-btn>
         </v-toolbar-items>
       </v-toolbar>
       <!-- Preview checklist selected and the user basic info... -->
-      <v-container fluid>
-        <v-row class="justify-center">
+      <v-container  class="fill-height" fluid>
+        <v-row align="center" justify="center">
           <v-col sm="6" md="6" cols="12">
-            <v-card id="defaultcard">
+            <v-card flat outlined>
               <v-card-title>CHECKLIST RESULT</v-card-title>
-              <v-card-subtitle><strong>09-13-2020</strong></v-card-subtitle>
-              <v-container fluid>
+              <v-card-subtitle class="font-italic">
+                <strong>09-13-2020</strong>
+              </v-card-subtitle>
+              <v-container>
                 <v-row class="ml-5">
                   <v-col cols="12">
                     <span>Purpose(s)</span><br>
-                    <span>- <strong class="text-uppercase">{{ purpose }}</strong></span>
+                    <span>- 
+                      <strong class="text-uppercase font-italic red--text">
+                        {{ purpose }}
+                      </strong>
+                    </span>
                   </v-col>
                 </v-row>
                 <v-row class="ml-5">
                   <v-col cols="12">
+                    <!-- SYMPTOMS ARRAYS DATA -->
                     <span>Symptoms</span><br>
                     <span>- 
                       <strong 
+                        class="font-italic red--text"
                         v-for="symptom in symptoms" :key="symptom"
                       >
                         {{ `${symptom}, ` }}
                       </strong>
-                      <strong>{{ noSymptoms }}</strong>
+                      <strong class="font-italic red--text">{{ noSymptoms }}</strong>
                     </span>
+
                   </v-col>
                 </v-row>
                 <v-row class="ml-5">
                   <v-col cols="12">
+                     <!-- TRAVEL HISTORY -->
                     <span>Travel History</span><br>
-                    <span>- 
-                      <strong 
-                        v-for="symptom in symptoms" :key="symptom"
-                      >
-                        {{ `${symptom}, ` }}
-                      </strong>
-                      <strong>{{ noSymptoms }}</strong>
-                    </span>
+                    <div class="ml-2">
+                      <small>Outside Country</small><br>
+                      <span>- 
+                        <strong
+                          class="font-italic red--text" 
+                          v-for="outside in travelOutsideCountry" :key="outside"
+                        >
+                          {{ `${outside}, ` }}
+                        </strong>
+                        <strong class="font-italic red--text" v-show="noTravelOusideCountry">
+                          {{ ` ${noTravelOusideCountry}, ` }}
+                        </strong>
+                        <strong class="font-italic red--text" v-show="othersTravelledOutsideCoutry">
+                          {{ ` ${othersTravelledOutsideCoutry} ` }}
+                        </strong>
+                      </span>
+                    </div>
+                    <div class="ml-2">
+                      <!-- TRAVEL LOCAL COUNTRY -->
+                      <small>Local Area</small><br>
+                      <span>-
+                        <strong 
+                          class="font-italic red--text"
+                          v-for="local in travelLocal" :key="local"
+                        >
+                          {{ `${local}, ` }}
+                        </strong>
+                        <strong class="font-italic red--text" v-show="noTravelLocal">
+                          {{ ` ${noTravelLocal}, ` }}
+                        </strong>
+                        <strong class="font-italic red--text" v-show="othersTravelledLocal">
+                          {{ ` ${othersTravelledLocal} ` }}
+                        </strong>
+                      </span>
+                    </div>
+                    <!-- END TRAVEL HISTORY -->
                   </v-col>
                 </v-row>
                 <v-row class="ml-5">
                   <v-col cols="12">
                     <span>Do you have a family members tested for covid-19 RT-PCR test?</span><br>
-                    <span>- <strong>No</strong></span>
+                    <span>- <strong class="font-italic red--text">{{ familyMemberTestedRTPCR }}</strong></span>
                   </v-col>
                 </v-row>
                 <v-row class="ml-5">
                   <v-col cols="12">
                     <span>Do you have a neighbors tested for covid-19 RT-PCR test?</span><br>
-                    <span>- <strong>No</strong></span>
+                    <span>- <strong class="font-italic red--text">{{ neighborTestedRTPCR }}</strong></span>
                   </v-col>
                 </v-row>
-                <hr>
+                <v-divider class="mx-5"></v-divider>
                 <v-row class="justify-center">
                   <v-col sm="12" md="12" cols="12">
-                    <div class="defaulttxt-p2 text-center">
-                      I hereby authorize SPMI, to collect and process the data indicated
-                      herein for the purpose of effecting control of the COVID-19 infection.
-                      I understand that my personal information is protected by 
-                      RA 10173, Data Privacy Act of 2012,
-                      and that I am required by RA 11469, Bayanihan to Heal as One Act,
-                      to provide truthful information. <br> <br>
-
-                      if you agree type your password to confirm.
+                    <div class="text-justify mx-5 gray--text font-weight-light subtitle-2 font-italic">
+                      I hereby authorize SPMI, to collect and process the data indicated herein for the purpose of effecting control of the COVID-19 infection. I understand that my personal information is protected by RA 10173, Data Privacy Act of 2012, and that I am required by RA 11469, Bayanihan to Heal as One Act, to provide truthful information. <br> 
                     </div>
                     <v-row class="justify-center">
                       <v-col sm="8" md="8" cols="12">
+                        <span class="body-2 font-weight-medium">If you agree type your password to confirm.</span>
                         <v-text-field
                           label="Password"
-                          type="password"
+                          :type="showPass ? 'text' : 'password'"
+                          :append-icon="showPass ? 'mdi-eye-off' : 'mdi-eye'"
+                          @click:append="showPass = !showPass"
                         ></v-text-field>
                       </v-col>
                     </v-row>
@@ -111,6 +145,11 @@
 <script>
   export default {
     name: 'preview-modal',
+    data () {
+      return {
+        showPass: false
+      }
+    },
     props: {
       visible: {
         type: Boolean,
@@ -118,7 +157,7 @@
       },
       purpose: {
         type: String,
-        required: true,
+        required: false,
       },
       noSymptoms: {
         type: String,
