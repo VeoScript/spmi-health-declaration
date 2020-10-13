92eafbaac6071b79266195abf2b67f26dc956645
acatzk <joshuaimalay@gmail.com+>
2020-10-13 17:30:11 +0800

    Import and displayed formatted_date

---
+++ src/components/mixins/DateFormat.vue
@@ -0,0 +1,22 @@
+<template>
+  <span>
+    {{ format_date(created_at) }}
+  </span>
+</template>
+
+<script>
+  import moment from 'moment'
+  export default {
+    props: {
+      created_at: {
+        type: String,
+        required: true
+      }
+    },
+    methods: {
+      format_date(value) {
+        return moment(String(value)).format('MM/D/YYYY')
+      }
+    }
+  }
+</script>
