8cee4ce4ace9928a25238ca2cd1b606f78510d8c
acatzk <joshuaimalay@gmail.com+>
2020-10-13 17:13:54 +0800

    Successfully displayed real time the daily-records data for health declaration

--- src/components/pages/home-daily-records/DailyRecordsTable.vue
+++ src/components/pages/home-daily-records/DailyRecordsTable.vue
@@ -1,20 +1,14 @@
 <template>
-  <v-card flat>
+  <v-card flat outlined>
     <v-card-title>
       Health Declaration
       <v-spacer></v-spacer>
-      <v-text-field
-        v-model="search"
-        append-icon="mdi-magnify"
-        label="Search"
-        single-line
-        hide-details
-      ></v-text-field>
+      <slot name="search-record"></slot>
     </v-card-title>
     <v-card-subtitle>Daily Records</v-card-subtitle>
     <v-data-table
       :headers="headers"
-      :items="dailyrecord"
+      :items="dailyRecords"
       :search="search"
     ></v-data-table>
   </v-card>
@@ -22,41 +16,19 @@
 
 <script>
   export default {
-    data () {
-      return {
-        search: '',
-        headers: [
-          {
-            text: 'DATE',
-            align: 'start',
-            sortable: false,
-            value: 'date',
-          },
-          { text: 'SYMPTOMS', value: 'symptoms' },
-          { text: 'TRAVEL HISTORY', value: 'travelhistory' },
-          { text: 'PURPOSE(S)', value: 'purpose' },
-        ],
-        dailyrecord: [
-          {
-            date: '09-13-2020',
-            symptoms: 'None of the Above',
-            travelhistory: 'None of the Above',
-            purpose: 'DUTY',
-          },
-          {
-            date: '09-14-2020',
-            symptoms: 'Fever, Headache, Sore Throat, Cough',
-            travelhistory: 'Metro Manila,  Tacloban City',
-            purpose: 'VISIT',
-          },
-          {
-            date: '09-15-2020',
-            symptoms: 'Headache',
-            travelhistory: 'Ormoc City',
-            purpose: 'DUTY',
-          },
-        ],
+    props: {
+      dailyRecords: {
+        type: Array,
+        required: true
+      },
+      search: {
+        type: String,
+        required: true
+      },
+      headers: {
+        type: Array,
+        required: true
       }
-    },
+    }
   }
 </script>
--- src/components/pages/home-daily-records/Index.vue
+++ src/components/pages/home-daily-records/Index.vue
@@ -2,16 +2,81 @@
   <div class="dailyrecords">
     <v-container class="mt-5" fluid>
       <!-- daily records table view -->
-      <daily-records-table />
+      <daily-records-table
+        :dailyRecords="checkList"
+        :search="search"
+        :headers="headers"
+      >
+        <template #search-record>
+          <v-text-field
+            v-model="search"
+            append-icon="mdi-magnify"
+            label="Search"
+            single-line
+            hide-details
+          ></v-text-field>
+        </template>
+      </daily-records-table>
     </v-container>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'dailyrecords',
-  components: {
-    DailyRecordsTable: () => import('@/components/pages/home-daily-records/DailyRecordsTable')
+  import { auth } from '@/services'
+  import { CHECKLIST_RESULT_QUERY } from '@/graphql/queries'
+  import { CHECKLIST_RESULT_SUBSCRIPTION } from '@/graphql/subscriptions'
+  export default {
+    name: 'dailyrecords',
+    components: {
+      DailyRecordsTable: () => import('@/components/pages/home-daily-records/DailyRecordsTable')
+    },
+    data () {
+      return {
+          search: '',
+          checkList: [],
+          headers: [
+            {
+              text: 'DATE',
+              align: 'start',
+              sortable: false,
+              value: 'created_at',
+            },
+            { text: 'SYMPTOMS', value: 'symptoms' },
+            { text: 'TRAVEL OUTSIDE COUNTRY', value: 'travel_outside_country' },
+            { text: 'TRAVEL LOCAL AREA', value: 'travel_local_country' },
+            { text: 'PURPOSE(S)', value: 'purpose' },
+          ]
+      }
+    },
+    apollo: {
+      checklist_result: {
+        query: CHECKLIST_RESULT_QUERY,
+        variables () {
+          return {
+            firebase_id: auth ? auth.currentUser.uid : undefined
+          }
+        },
+        subscribeToMore: {
+          document: auth ? CHECKLIST_RESULT_SUBSCRIPTION : undefined,
+          variables () {
+            return {
+              firebase_id: auth ? auth.currentUser.uid : undefined
+            }
+          },
+          updateQuery(previousResult, { subscriptionData }) {
+            if (previousResult) {
+              return {
+                checklist_result: [
+                    ...subscriptionData.data.checklist_result
+                ]
+              }
+            }
+          }
+        },
+        result ({ data }) {
+          this.checkList = data.checklist_result
+        }
+      }
+    }
   }
-}
 </script>
