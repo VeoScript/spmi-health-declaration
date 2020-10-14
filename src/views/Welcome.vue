5ba4850fd70f975fa0db9d9267d0053b87704657
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2020-10-14 13:54:31 +0800

    added address in welcome page

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -3,9 +3,10 @@
     <v-container class="fill-height" fluid>
       <v-row align="center" justify="center">
         <v-card flat>
-          <v-card-text>
+          <v-card-text class="text-right">
             <v-img src='@/assets/spmi_official.png' max-width="450"/>
-            <h2>Covid-19 Health Declaration</h2>
+            <p>BRGY. HILAPNITAN, BAYBAY CITY, LEYTE</p>
+            <h2>COVID-19 HEALTH DECLARATION</h2>
           </v-card-text>
           <v-card-actions>
             <v-btn 
