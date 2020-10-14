f4d3da0ba35767ed4c5c166363c96ee5dd434c0e
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2020-10-14 10:19:24 +0800

    fixed graphql mutations index.js

--- src/graphql/mutations/index.js
+++ src/graphql/mutations/index.js
@@ -1,5 +1,7 @@
 import { ADD_USERS_MUTATION } from './users'
+import { ADD_HEALTH_DECLARATION_MUTATION } from './checklist_result'
 
 export {
-    ADD_USERS_MUTATION
+    ADD_USERS_MUTATION,
+    ADD_HEALTH_DECLARATION_MUTATION
 }
