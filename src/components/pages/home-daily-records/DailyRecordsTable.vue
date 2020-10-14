628909aa77f8f620599f538a91dd8b3ff7fd182b
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2020-10-14 10:24:14 +0800

    fixed home daily records DataTable

--- src/components/pages/home-daily-records/DailyRecordsTable.vue
+++ src/components/pages/home-daily-records/DailyRecordsTable.vue
@@ -1,62 +1,11 @@
 <template>
-  <v-card flat>
+  <v-card flat outlined>
     <v-card-title>
       Health Declaration
       <v-spacer></v-spacer>
-      <v-text-field
-        v-model="search"
-        append-icon="mdi-magnify"
-        label="Search"
-        single-line
-        hide-details
-      ></v-text-field>
+      <slot name="search-record"></slot>
     </v-card-title>
     <v-card-subtitle>Daily Records</v-card-subtitle>
-    <v-data-table
-      :headers="headers"
-      :items="dailyrecord"
-      :search="search"
-    ></v-data-table>
+    <slot name="data-records"></slot>
   </v-card>
 </template>
-
-<script>
-  export default {
-    data () {
-      return {
-        search: '',
-        headers: [
-          {
-            text: 'DATE',
-            align: 'start',
-            sortable: false,
-            value: 'date',
-          },
-          { text: 'SYMPTOMS', value: 'symptoms' },
-          { text: 'TRAVEL HISTORY', value: 'travelhistory' },
-          { text: 'PURPOSE(S)', value: 'purpose' },
-        ],
-        dailyrecord: [
-          {
-            date: '09-13-2020',
-            symptoms: 'None of the Above',
-            travelhistory: 'None of the Above',
-            purpose: 'DUTY',
-          },
-          {
-            date: '09-14-2020',
-            symptoms: 'Fever, Headache, Sore Throat, Cough',
-            travelhistory: 'Metro Manila,  Tacloban City',
-            purpose: 'VISIT',
-          },
-          {
-            date: '09-15-2020',
-            symptoms: 'Headache',
-            travelhistory: 'Ormoc City',
-            purpose: 'DUTY',
-          },
-        ],
-      }
-    },
-  }
-</script>
