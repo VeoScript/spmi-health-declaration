dc596c8e2910f70c2c9ab34151b0b027f8bf41af
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2020-10-14 10:18:33 +0800

    fixed graphql queries index.js

--- src/graphql/queries/index.js
+++ src/graphql/queries/index.js
@@ -1,6 +1,8 @@
 import { GET_ACCOUNT_TYPE_QUERY, GET_USER_BASIC_INFO } from './users'
+import { CHECKLIST_RESULT_QUERY } from './checklist_result'
 
 export {
     GET_ACCOUNT_TYPE_QUERY,
-    GET_USER_BASIC_INFO
+    GET_USER_BASIC_INFO,
+    CHECKLIST_RESULT_QUERY
 }
