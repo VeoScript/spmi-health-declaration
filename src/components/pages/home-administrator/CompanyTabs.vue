f394ca40489e615a8e735bfa405ffd369eff4ee8
acatzk <joshuaimalay@gmail.com+>
2020-10-14 21:25:01 +0800

    Passed companies Array and naming slot by each company name

---
+++ src/components/pages/home-administrator/CompanyTabs.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <v-tabs
+      v-model="tab"
+      grow
+      background-color="transparent"
+    >
+      <v-tab
+        v-for="company in companies" :key="company"
+      >
+        {{ company }}
+      </v-tab>
+    </v-tabs>
+    <v-tabs-items v-model="tab">
+      <v-tab-item 
+        v-for="company in companies" :key="company"
+      >
+        <v-card flat>
+          <slot :name="`${company}-company`"></slot>
+        </v-card>
+      </v-tab-item>
+    </v-tabs-items>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      companies: {
+        type: Array,
+        required: true
+      }
+    },
+    data () {
+      return {
+        tab: null
+      }
+    }
+  }
+</script>
