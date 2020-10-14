fd491c8a969c52ed11f370f4c99b9dc4a66d0c16
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2020-10-14 10:23:13 +0800

    changed CheckListPreviewDialog to CheckListDialog

--- src/components/pages/home-health-check-list/CheckListDialog.vue
+++ src/components/pages/home-health-check-list/CheckListDialog.vue
@@ -1,96 +1,70 @@
 <template>
   <v-dialog
     class="preview-modal"
-    v-model="dialog"
     fullscreen
     hide-overlay
     transition="dialog-bottom-transition"
+    v-model="show"
   >
-    <template v-slot:activator="{ on, attrs }">
-      <v-btn
-        color="primary"
-        v-bind="attrs"
-        v-on="on"
-      >
-        Preview
-      </v-btn>
-    </template>
-    <v-card style="background: #fff;">
+    <v-card class="gray">
       <v-toolbar class="dgbox">
         <v-btn
           icon
-          @click="dialog = false"
+          @click="show = !show"
         >
           <v-icon>mdi-close</v-icon>
         </v-btn>
         <v-toolbar-title>Preview Health Checklist</v-toolbar-title>
         <v-spacer></v-spacer>
         <v-toolbar-items>
-          <v-btn
-            text
-            @click="dialog = false"
-          >
-            Save
-          </v-btn>
+          <slot name="btn-save-health"></slot>
         </v-toolbar-items>
       </v-toolbar>
+      <v-alert
+        border="bottom"
+        color="error darken-1"
+        dark
+        dismissible
+        v-show="error"
+        type="error"
+      >
+        {{ error }}
+      </v-alert>
       <!-- Preview checklist selected and the user basic info... -->
-      <v-container fluid>
-        <v-row class="justify-center">
+      <v-container  class="fill-height" fluid>
+        <v-row align="center" justify="center">
           <v-col sm="6" md="6" cols="12">
-            <v-card outlined id="defaultcard">
+            <v-card flat outlined>
               <v-card-title>CHECKLIST RESULT</v-card-title>
-              <v-card-subtitle><strong>09-13-2020</strong></v-card-subtitle>
-              <v-container fluid>
+              <v-card-subtitle class="font-italic">
+                <strong>
+                  {{ getCurrentDate }}
+                </strong>
+              </v-card-subtitle>
+              <v-container>
+                <slot name="purpose"></slot>
+                <slot name="symptoms"></slot>
+                <slot name="travel-history"></slot>
                 <v-row class="ml-5">
                   <v-col cols="12">
-                    <span>Purpose(s)</span><br>
-                    <span>- <strong>DUTY</strong></span>
+                    <slot name="family-tested"></slot>
                   </v-col>
                 </v-row>
                 <v-row class="ml-5">
                   <v-col cols="12">
-                    <span>Symptoms</span><br>
-                    <span>- <strong>None of the Above</strong></span>
+                    <slot name="neighbor-tested"></slot>
                   </v-col>
                 </v-row>
-                <v-row class="ml-5">
-                  <v-col cols="12">
-                    <span>Travel History</span><br>
-                    <span>- <strong>None of the Above</strong></span>
-                  </v-col>
-                </v-row>
-                <v-row class="ml-5">
-                  <v-col cols="12">
-                    <span>Do you have a family members tested for covid-19 RT-PCR test?</span><br>
-                    <span>- <strong>No</strong></span>
-                  </v-col>
-                </v-row>
-                <v-row class="ml-5">
-                  <v-col cols="12">
-                    <span>Do you have a neighbors tested for covid-19 RT-PCR test?</span><br>
-                    <span>- <strong>No</strong></span>
-                  </v-col>
-                </v-row>
-                <hr>
+                <v-divider class="mx-5"></v-divider>
                 <v-row class="justify-center">
                   <v-col sm="12" md="12" cols="12">
-                    <div class="defaulttxt-p2 text-center">
-                      I hereby authorize SPMI, to collect and process the data idicated
-                      herein for the purpose of effecting control of the COVID-19 infection.
-                      I understand that my personal information is protected by 
-                      RA 10173, Data Privacy Act of 2012,
-                      and that I am required by RA 11469, Bayanihan to Heal as One Act,
-                      to provide truthful information. <br> <br>
-
-                      if you agree type your password to confirm.
+                    <div class="text-justify mx-5 gray--text font-weight-light subtitle-2 font-italic">
+                      I hereby authorize SPMI, to collect and process the data indicated herein for the purpose of effecting control of the COVID-19 infection. I understand that my personal information is protected by RA 10173, Data Privacy Act of 2012, and that I am required by RA 11469, Bayanihan to Heal as One Act, to provide truthful information. <br> 
                     </div>
                     <v-row class="justify-center">
                       <v-col sm="8" md="8" cols="12">
-                        <v-text-field
-                          label="Password"
-                          type="password"
-                        ></v-text-field>
+                        <span class="body-2 font-weight-medium">If you agree type your password to confirm.</span>
+                        <slot name="confirm-password"></slot>
                       </v-col>
                     </v-row>
                   </v-col>
@@ -108,18 +82,35 @@
   export default {
     name: 'preview-modal',
     props: {
-      user: {
-        type: Object,
+      visible: {
+        type: Boolean,
         required: true
+      },
+      error: {
+        type: String,
+        required: false,
+        default: ''
       }
     },
-    data () {
-      return {
-        dialog: false,
-        notifications: false,
-        sound: true,
-        widgets: false,
+    computed: {
+      show: {
+        get () {
+          return this.visible
+        },
+        set (value) {
+          if (!value) {
+            this.$emit('close-dialog')
+          }
+        }
+      },
+      getCurrentDate () {
+        var today = new Date();
+        var dd = String(today.getDate()).padStart(2, '0');
+        var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        var yyyy = today.getFullYear();
+        today = mm + '/' + dd + '/' + yyyy;
+       return today
       }
-    },
+    }
   }
 </script>
