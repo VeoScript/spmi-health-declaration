93f23c9d999adede12902af66d5124c5c77f24a6
acatzk <joshuaimalay@gmail.com+>
2020-10-17 07:06:40 +0800

    Added brgy in the welcome page

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -3,8 +3,9 @@
     <v-container class="fill-height" fluid>
       <v-row align="center" justify="center">
         <v-card flat>
-          <v-card-text>
+          <v-card-text class="text-right">
             <v-img src='@/assets/spmi_official.png' max-width="450"/>
+            <p>BRGY. HILAPNITAN, BAYBAY CITY, LEYTE</p>
             <h2>Covid-19 Health Declaration</h2>
           </v-card-text>
           <v-card-actions>
