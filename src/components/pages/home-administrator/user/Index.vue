124fa00796b647c65f90cb4dbe96f4201bdfcd62
acatzk <joshuaimalay@gmail.com+>
2020-10-17 10:00:20 +0800

    Added layout and import UserProfile and UserResult

---
+++ src/components/pages/home-administrator/user/Index.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col cols="12" md="7" sm="7">
+        <user-result />
+      </v-col>
+      <v-divider vertical inset></v-divider>
+      <v-col cols="12" md="4" sm="7">
+        <user-profile />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    components: {
+      UserProfile: () => import('./UserProfile'),
+      UserResult: () => import('./UserResult')
+    }
+  }
+</script>
