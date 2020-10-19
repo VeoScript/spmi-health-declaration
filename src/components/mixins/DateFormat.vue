9c841f2855de79475fa437a53130c56eff315282
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2020-10-19 15:22:23 +0800

    changed date format

--- src/components/mixins/DateFormat.vue
+++ src/components/mixins/DateFormat.vue
@@ -15,7 +15,7 @@
     },
     methods: {
       format_date(value) {
-        return moment(String(value)).format('LL')
+        return moment(String(value)).format('MM/D/YYYY - h:mm A')
       }
     }
   }
