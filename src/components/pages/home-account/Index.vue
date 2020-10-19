f78a2bb8a13d984914db7f759f79a9cf01261ab4
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2020-10-19 15:22:39 +0800

    created account layout

--- src/components/pages/home-account/Index.vue
+++ src/components/pages/home-account/Index.vue
@@ -1,5 +1,88 @@
 <template>
-  <div>
-      <h1>Account</h1>
-  </div>
+  <v-row >
+    <v-col sm="5" md="5" cols="12">
+      <v-card outlined>
+        <v-card-title>Account</v-card-title>
+        <v-card-subtitle>Main Information</v-card-subtitle>
+        <v-divider></v-divider>
+        <v-row class="mx-1 mt-2">
+          <v-col sm="12" md="12" cols="12">
+            <v-card outlined color="gray lighten-3" class="px-2 py-2">
+              <div class="caption">Name</div>
+              <div class="body-2">JEROME JOSEPH R. VILLARUEL</div>
+            </v-card>
+          </v-col>
+          <v-col sm="12" md="12" cols="12">
+            <v-card outlined color="gray lighten-3" class="px-2 py-2">
+              <div class="caption">Occupation</div>
+              <div class="body-2">IT COORDINATOR</div>
+            </v-card>
+          </v-col>
+          <v-col sm="6" md="6" cols="12">
+            <v-card outlined color="gray lighten-3" class="px-2 py-2">
+              <div class="caption">Company</div>
+              <div class="body-2">SPMI</div>
+            </v-card>
+          </v-col>
+          <v-col sm="6" md="6" cols="12">
+            <v-card outlined color="gray lighten-3" class="px-2 py-2">
+              <div class="caption">Department</div>
+              <div class="body-2">OPM</div>
+            </v-card>
+          </v-col>
+          <v-col sm="12" md="12" cols="12" class="text-right">
+            <v-btn outlined tile small color="success"><v-icon>mdi-pencil</v-icon> Edit</v-btn>
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-col>
+    <v-col sm="7" md="7" cols="12">
+      <v-card outlined>
+        <v-card-title>Other Information</v-card-title>
+        <v-card-subtitle>Basic Information and User Account</v-card-subtitle>
+        <v-divider></v-divider>
+        <v-row class="mx-1 mt-2">
+          <v-col sm="4" md="4" cols="12">
+            <v-card outlined color="gray lighten-3" class="px-2 py-2">
+              <div class="caption">Age</div>
+              <div class="body-2">22</div>
+            </v-card>
+          </v-col>
+          <v-col sm="4" md="4" cols="12">
+            <v-card outlined color="gray lighten-3" class="px-2 py-2">
+              <div class="caption">Gender</div>
+              <div class="body-2">MALE</div>
+            </v-card>
+          </v-col>
+          <v-col sm="4" md="4" cols="12">
+            <v-card outlined color="gray lighten-3" class="px-2 py-2">
+              <div class="caption">Nationality</div>
+              <div class="body-2">Filipino</div>
+            </v-card>
+          </v-col>
+          <v-col sm="6" md="6" cols="12">
+            <v-card outlined color="gray lighten-3" class="px-2 py-2">
+              <div class="caption">Civil Status</div>
+              <div class="body-2">SINGLE</div>
+            </v-card>
+          </v-col>
+          <v-col sm="6" md="6" cols="12">
+            <v-card outlined color="gray lighten-3" class="px-2 py-2">
+              <div class="caption">Contact Number</div>
+              <div class="body-2">09753286466</div>
+            </v-card>
+          </v-col>
+          <v-col sm="12" md="12" cols="12">
+            <v-card outlined color="gray lighten-3" class="px-2 py-2">
+              <div class="caption">Email</div>
+              <div class="body-2">jeromevillaruel1998@yahoo.com</div>
+            </v-card>
+          </v-col>
+          <v-col sm="12" md="12" cols="12" class="text-right">
+            <v-btn outlined tile small color="success"><v-icon>mdi-pencil</v-icon> Edit</v-btn>
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
