557999906af9ccae20a99f1ff2e74537d5862a19
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2020-10-19 18:21:04 +0800

    added index.vue, modified back-end

--- src/components/pages/home-account/Index.vue
+++ src/components/pages/home-account/Index.vue
@@ -1,88 +1,61 @@
 <template>
-  <v-row >
-    <v-col sm="5" md="5" cols="12">
-      <v-card outlined>
-        <v-card-title>Account</v-card-title>
-        <v-card-subtitle>Main Information</v-card-subtitle>
-        <v-divider></v-divider>
-        <v-row class="mx-1 mt-2">
-          <v-col sm="12" md="12" cols="12">
-            <v-card outlined color="gray lighten-3" class="px-2 py-2">
-              <div class="caption">Name</div>
-              <div class="body-2">JEROME JOSEPH R. VILLARUEL</div>
-            </v-card>
-          </v-col>
-          <v-col sm="12" md="12" cols="12">
-            <v-card outlined color="gray lighten-3" class="px-2 py-2">
-              <div class="caption">Occupation</div>
-              <div class="body-2">IT COORDINATOR</div>
-            </v-card>
-          </v-col>
-          <v-col sm="6" md="6" cols="12">
-            <v-card outlined color="gray lighten-3" class="px-2 py-2">
-              <div class="caption">Company</div>
-              <div class="body-2">SPMI</div>
-            </v-card>
-          </v-col>
-          <v-col sm="6" md="6" cols="12">
-            <v-card outlined color="gray lighten-3" class="px-2 py-2">
-              <div class="caption">Department</div>
-              <div class="body-2">OPM</div>
-            </v-card>
-          </v-col>
-          <v-col sm="12" md="12" cols="12" class="text-right">
-            <v-btn outlined tile small color="success"><v-icon>mdi-pencil</v-icon> Edit</v-btn>
-          </v-col>
-        </v-row>
-      </v-card>
-    </v-col>
-    <v-col sm="7" md="7" cols="12">
-      <v-card outlined>
-        <v-card-title>Other Information</v-card-title>
-        <v-card-subtitle>Basic Information and User Account</v-card-subtitle>
-        <v-divider></v-divider>
-        <v-row class="mx-1 mt-2">
-          <v-col sm="4" md="4" cols="12">
-            <v-card outlined color="gray lighten-3" class="px-2 py-2">
-              <div class="caption">Age</div>
-              <div class="body-2">22</div>
-            </v-card>
-          </v-col>
-          <v-col sm="4" md="4" cols="12">
-            <v-card outlined color="gray lighten-3" class="px-2 py-2">
-              <div class="caption">Gender</div>
-              <div class="body-2">MALE</div>
-            </v-card>
-          </v-col>
-          <v-col sm="4" md="4" cols="12">
-            <v-card outlined color="gray lighten-3" class="px-2 py-2">
-              <div class="caption">Nationality</div>
-              <div class="body-2">Filipino</div>
-            </v-card>
-          </v-col>
-          <v-col sm="6" md="6" cols="12">
-            <v-card outlined color="gray lighten-3" class="px-2 py-2">
-              <div class="caption">Civil Status</div>
-              <div class="body-2">SINGLE</div>
-            </v-card>
-          </v-col>
-          <v-col sm="6" md="6" cols="12">
-            <v-card outlined color="gray lighten-3" class="px-2 py-2">
-              <div class="caption">Contact Number</div>
-              <div class="body-2">09753286466</div>
-            </v-card>
-          </v-col>
-          <v-col sm="12" md="12" cols="12">
-            <v-card outlined color="gray lighten-3" class="px-2 py-2">
-              <div class="caption">Email</div>
-              <div class="body-2">jeromevillaruel1998@yahoo.com</div>
-            </v-card>
-          </v-col>
-          <v-col sm="12" md="12" cols="12" class="text-right">
-            <v-btn outlined tile small color="success"><v-icon>mdi-pencil</v-icon> Edit</v-btn>
-          </v-col>
-        </v-row>
-      </v-card>
-    </v-col>
-  </v-row>
+  <div>
+    <v-skeleton-loader
+      v-if="$apollo.loading"
+      type="list-item-avatar-two-line"
+    ></v-skeleton-loader> <!--apollo loading when data is fetching --> 
+    <user-account
+      v-else
+      v-for="user in users" 
+      :key="user.id"
+      :user="user"
+    ></user-account>
+  </div>
 </template>
+
+<script>
+  import { auth } from '@/services'
+  import { GET_USER_BASIC_INFO } from '@/graphql/queries'
+  import { GET_USER_BASIC_INFO_SUBSCRIPTION } from '@/graphql/subscriptions'
+
+export default {
+  components: {
+    UserAccount: () => import('./UserAccount')
+  },
+  data () {
+    return {
+      users: [],
+    }
+  },
+    apollo: {
+      users: {
+        query: auth ? GET_USER_BASIC_INFO : undefined,
+        variables () {
+          return {
+            firebase_id: auth ? auth.currentUser.uid : undefined
+          }
+        },
+        subscribeToMore: {
+          document: auth ? GET_USER_BASIC_INFO_SUBSCRIPTION : undefined,
+          variables () {
+            return {
+              firebase_id: auth ? auth.currentUser.uid : undefined
+            }
+          },
+          updateQuery(previousResult, { subscriptionData }) {
+            if (previousResult) {
+              return {
+                users: [
+                    ...subscriptionData.data.users
+                ]
+              }
+            }
+          }
+        },
+        result ({ data }) {
+          this.users = data.users
+        }
+      }
+    }
+}
+</script>
