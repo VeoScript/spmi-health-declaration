1099866ba838a2852c05e860b8c105765a02f49d
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2020-10-19 09:27:44 +0800

    update date format

--- src/components/pages/home-health-check-list/CheckListDialog.vue
+++ src/components/pages/home-health-check-list/CheckListDialog.vue
@@ -108,6 +108,7 @@
         var dd = String(today.getDate()).padStart(2, '0');
         var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
         var yyyy = today.getFullYear();
+
         today = mm + '/' + dd + '/' + yyyy;
        return today
       }
