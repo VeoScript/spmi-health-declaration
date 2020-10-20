fbbe9d65062a6c74d80207641c3deef6134fb4af
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2020-10-20 14:46:13 +0800

    created editmaininformation.vue component

---
+++ src/components/pages/home-account/EditMainInformation.vue
@@ -0,0 +1,119 @@
+<template>
+  <v-row justify="end">
+    <v-dialog
+      v-model="dialog"
+      persistent
+      max-width="600px"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          outlined
+          tile
+          small
+          color="success"
+          v-bind="attrs"
+          v-on="on"
+        >
+          <v-icon>mdi-pencil</v-icon> Edit
+        </v-btn>
+      </template>
+      <v-card>
+        <v-card-title>
+          <span class="headline">Edit Main Information</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col
+                cols="12"
+                sm="6"
+                md="4"
+              >
+                <v-text-field
+                  label="Last Name"
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col
+                cols="12"
+                sm="6"
+                md="4"
+              >
+                <v-text-field
+                  label="First Name"
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col
+                cols="12"
+                sm="6"
+                md="4"
+              >
+                <v-text-field
+                  label="Middle Name"
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                  label="Occupation"
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col
+                cols="12"
+                sm="6"
+              >
+                <v-select
+                  label="Company"
+                  :items="company"
+                  required
+                ></v-select>
+              </v-col>
+              <v-col
+                cols="12"
+                sm="6"
+              >
+                <v-select
+                  label="Department"
+                  :items="department"
+                  required
+                ></v-select>
+              </v-col>
+            </v-row>
+          </v-container>
+          <small>Be responsible for your correct information.</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="blue darken-1"
+            text
+            @click="dialog = false"
+          >
+            Close
+          </v-btn>
+          <v-btn
+            color="blue darken-1"
+            text
+            @click="dialog = false"
+          >
+            Save
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        dialog: false,
+        company: ['SPMI', 'VACI', 'GGC', 'SECURITY'],
+        department: ['ADMIN', 'OPM', 'PRODUCTION', 'QUALITY CONTROL', 'TECHNICAL']
+      }
+    },
+  }
+</script>
