20f408686b5bd0f8bf4d9858344f6c803f626cb8
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2020-10-22 11:15:42 +0800

    add truthful information in about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,8 +2,20 @@
   <div class="about">
     <v-container class="fluid mt-3 text-center">
       <v-container class="fluid">
-         <small>Still working...</small>
-        <h1>GOD bless you!</h1>
+        <h1>ABOUT</h1>
+        <v-row>
+          <v-col sm="12" md="12" cols="12">
+            <v-card flat>
+              <div>
+                This system is for Covid-19 Health Declaration <br> program herein Specialty Pulp Manufacturing, Inc. (SPMI).<br>
+                To monitor the covid-19 cases in SPMI Plant and Baybay City, Leyte, <br>
+                this is compulsory for all of employees in SPMI and the employees under agencies. <br><br>
+                This project is licensed under the MIT License and copyrighted by the company. <br>
+              </div>
+              <small>Designed and Developed by Team veoscript & acatzk.</small>
+            </v-card>
+          </v-col>
+        </v-row>
       </v-container>
     </v-container>
   </div>
