404137f4b337eda106db875c6f90667859112016
acatzk <joshuaimalay@gmail.com+>
2020-10-22 15:06:31 +0800

    Removed the age and replaced to birthday field

--- src/graphql/mutations/users.js
+++ src/graphql/mutations/users.js
@@ -1,8 +1,8 @@
 import gql from 'graphql-tag'
 
 export const ADD_USERS_MUTATION = gql`
-  mutation addUserMutation($firebase_id: String!, $lastname: String, $firstname: String, $middlename: String, $age: String, $gender: String, $civil_status: String, $contact_number: String, $occupation: String, $department: String, $company: String, $email: String, $password: String, $nationality: String, $address: String) {
-    insert_users(objects: {firebase_id: $firebase_id, lastname: $lastname, firstname: $firstname, middlename: $middlename, age: $age, gender: $gender, civil_status: $civil_status, contact_number: $contact_number, occupation: $occupation, department: $department, company: $company, email: $email, password: $password, nationality: $nationality, address: $address}) {
+  mutation addUserMutation($firebase_id: String!, $lastname: String, $firstname: String, $middlename: String, $gender: String, $civil_status: String, $contact_number: String, $occupation: String, $department: String, $company: String, $email: String, $password: String, $nationality: String, $address: String, $birthday: String) {
+    insert_users(objects: {firebase_id: $firebase_id, lastname: $lastname, firstname: $firstname, middlename: $middlename, gender: $gender, civil_status: $civil_status, contact_number: $contact_number, occupation: $occupation, department: $department, company: $company, email: $email, password: $password, nationality: $nationality, address: $address, birthday: $birthday}) {
       affected_rows
       returning {
           id
