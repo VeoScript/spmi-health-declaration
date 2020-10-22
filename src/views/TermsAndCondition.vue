b6f6ace3ca8537ada6852d8a361c70fa2349d0fb
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2020-10-22 11:16:02 +0800

    added truthful information in terms and condition page

---
+++ src/views/TermsAndCondition.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="about">
+    <v-container class="fluid mt-3 text-center">
+      <v-container class="fluid">
+        <h1>TERMS AND CONDITIONS</h1>
+        <v-row>
+          <v-col sm="12" md="12" cols="12">
+            <v-card flat>
+              <div>
+                I hereby authorize SPMI, to collect and process
+                the data indicated herein for the purpose of effecting control <br>
+                of the COVID-19 infection. I understand that my personal information is protected by RA 10173, <br>
+                Data Privacy Act of 2012, and that I am required by <br>
+                RA 11469, Bayanihan to Heal as One Act, to provide truthful information. <br><br>
+                This system is only for the SPMI Employees and the employees under agencies. <br>
+                Any kinds of false information registered herein the program can cause a crime under the cyber law. <br>
+                ©{{ new Date().getFullYear() }} Covid-19 Health Declaration, Specialty Pulp Manufacturing Inc. All rights reserved.
+              </div>
+              <!-- <small>Designed and Developed by Team veoscript & acatzk.</small> -->
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-container>
+  </div>
+</template>
