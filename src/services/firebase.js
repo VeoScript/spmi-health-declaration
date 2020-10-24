fb8827536ad95a5a541472ad2666fd86124d656a
acatzk <joshuaimalay@gmail.com+>
2020-10-24 15:59:59 +0800

    Hide confidential configuration

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,14 +3,14 @@ import 'firebase/storage'
 import 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAIc14vLU2aCeq2qYhwBdUCG1OAAGzE0bI",
-    authDomain: "spmi-health-declaration.firebaseapp.com",
-    databaseURL: "https://spmi-health-declaration.firebaseio.com",
-    projectId: "spmi-health-declaration",
-    storageBucket: "spmi-health-declaration.appspot.com",
-    messagingSenderId: "799466451744",
-    appId: "1:799466451744:web:cf78291ef6d61d13e9dd45",
-    measurementId: "G-VF2R9GJB3N"
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
+  measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID
 }
 
 // firebase utils
