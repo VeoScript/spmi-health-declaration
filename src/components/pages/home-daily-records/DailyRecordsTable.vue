9b0afceaa3cf6ee7e36dde01c890debb2d2d7088
acatzk <joshuaimalay@gmail.com+>
2020-10-25 07:47:20 +0800

    Added v-hover functionality with condition elevation

--- src/components/pages/home-daily-records/DailyRecordsTable.vue
+++ src/components/pages/home-daily-records/DailyRecordsTable.vue
@@ -1,11 +1,13 @@
 <template>
-  <v-card flat outlined style="min-height: 100vh;">
-    <v-card-title>
-      Health Declaration
-      <v-spacer></v-spacer>
-      <slot name="search-record"></slot>
-    </v-card-title>
-    <v-card-subtitle>Daily Records</v-card-subtitle>
-    <slot name="data-records"></slot>
-  </v-card>
+  <v-hover v-slot="{ hover }">
+    <v-card flat style="min-height: 100vh;" :elevation="hover ? 12 : 1">
+      <v-card-title>
+        Health Declaration
+        <v-spacer></v-spacer>
+        <slot name="search-record"></slot>
+      </v-card-title>
+      <v-card-subtitle>Daily Records</v-card-subtitle>
+      <slot name="data-records"></slot>
+    </v-card>
+  </v-hover>
 </template>
