f560d66c674550ac97b6f5b3bfca293ec6c6e151
acatzk <joshuaimalay@gmail.com+>
2020-10-25 08:44:04 +0800

    Added slot prop name

--- src/components/pages/home-account/UserDetails.vue
+++ src/components/pages/home-account/UserDetails.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <v-row align="center" justify="center" class="mb-3">
-      <slot name="title"></slot>
+      <slot name="user-title"></slot>
     </v-row>
     <slot name="user-text-fields"></slot>
   </div>
