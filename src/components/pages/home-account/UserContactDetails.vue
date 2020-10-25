fd8e9612adb2d68dfa81ead09bf3d875151302f7
acatzk <joshuaimalay@gmail.com+>
2020-10-25 08:43:45 +0800

    Removed additional component slots

--- src/components/pages/home-account/UserContactDetails.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <v-row align="center" justify="center" class="mb-3">
-      <slot name="info-details"></slot>
-    </v-row>
-    <slot name="user-contact"></slot>
-  </div>
-</template>
