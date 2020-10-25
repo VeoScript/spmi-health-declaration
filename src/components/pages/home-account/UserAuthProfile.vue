224d2898c87c764bd68094e7d5b0ffae30630a97
acatzk <joshuaimalay@gmail.com+>
2020-10-25 08:30:37 +0800

    Added slot name to pass content

---
+++ src/components/pages/home-account/UserAuthProfile.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-row align="center" justify="center">
+    <v-avatar
+      size="100"
+      style="border: 2px solid #eee;"
+    >
+      <v-img src="@/assets/imgs/male.jpg"></v-img>
+    </v-avatar>
+    <v-card flat class="ml-2 justify-center">
+      <v-card-title class="text-uppercase font-weight-bold justify-left">
+        Joshua A. Galit
+      </v-card-title>
+      <v-card-subtitle>
+        <v-btn depressed small class="text-capitalize" text outlined>
+          Edit Auth
+        </v-btn>
+      </v-card-subtitle>
+    </v-card>
+  </v-row>
+</template>
---
+++ src/components/pages/home-account/UserContactDetails.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <v-row align="center" justify="center" class="mb-3">
+      <slot name="info-details"></slot>
+    </v-row>
+    <slot name="user-contact"></slot>
+  </div>
+</template>
