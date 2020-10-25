a88c5b32be3d0a4713df0c90da83e89134ccc48d
acatzk <joshuaimalay@gmail.com+>
2020-10-25 08:35:42 +0800

    Creat UserDetails with pass prop name

---
+++ src/components/pages/home-account/UserDetails.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <v-row align="center" justify="center" class="mb-3">
+      <slot name="title"></slot>
+    </v-row>
+    <slot name="user-text-fields"></slot>
+  </div>
+</template>
