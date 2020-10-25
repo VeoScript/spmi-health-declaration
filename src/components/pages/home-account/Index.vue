72d4ebe6b3b63cafa0482447d51a873395edd59c
acatzk <joshuaimalay@gmail.com+>
2020-10-25 09:59:02 +0800

    Added validate rules and event triggered

--- src/components/pages/home-account/Index.vue
+++ src/components/pages/home-account/Index.vue
@@ -29,167 +29,196 @@
            justify="center">
       <v-col cols="12">
         
-        <user-details :users="users">
-          <template #user-title>
-            <div class="gray--text text-uppercase font-weight-bold">
-              Personal Details
-            </div>
-          </template>
-          <template #user-text-fields="{ user }">
-            <v-row align="center" justify="center" class="mb-3">
-              <v-col cols="12" md="3">
-                <v-text-field
-                  label="Firstname"
-                  outlined
-                  class="rounded-sm"
-                  v-model="user.firstname"
-                  text
-                ></v-text-field>
-              </v-col>
-              <v-col cols="12" md="3">
-                <v-text-field
-                  label="Middlename"
-                  outlined
-                  class="rounded-sm"
-                  v-model="user.middlename"
-                ></v-text-field>
-              </v-col>
-              <v-col cols="12" md="3">
-                <v-text-field
-                  label="Lastname"
-                  outlined
-                  class="rounded-sm"
-                  v-model="user.lastname"
-                ></v-text-field>
-              </v-col>
-            </v-row>
-            <v-row align="center" justify="center" class="mb-3">
-              <v-col cols="12" md="3">
-                <v-select
-                  :items="['Male', 'Female']"
-                  label="Gender"
-                  outlined
-                  v-model="user.gender"
-                  class="rounded-sm"
-                ></v-select>
-              </v-col>
-              <v-col cols="12" md="3">
-                <v-menu
-                  ref="menu"
-                  v-model="menu"
-                  :close-on-content-click="false"
-                  transition="scale-transition"
-                  offset-y
-                  min-width="290px"
-                >
-                  <template v-slot:activator="{ on, attrs }">
-                    <v-text-field
+        <v-form ref="form">
+          <user-details :users="users">
+            <template #user-title>
+              <div class="gray--text text-uppercase font-weight-bold">
+                Personal Details
+              </div>
+            </template>
+            <template #user-text-fields="{ user }">
+              <v-row align="center" justify="center" class="mb-3">
+                <v-col cols="12" md="3">
+                  <v-text-field
+                    label="Firstname"
+                    outlined
+                    class="rounded-sm"
+                    v-model="user.firstname"
+                    :rules="[required('Firstname')]"
+                    :disabled="loading"
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" md="3">
+                  <v-text-field
+                    label="Middlename"
+                    outlined
+                    :disabled="loading"
+                    class="rounded-sm"
+                    v-model="user.middlename"
+                    :rules="[required('Middlename')]"
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" md="3">
+                  <v-text-field
+                    label="Lastname"
+                    outlined
+                    :disabled="loading"
+                    class="rounded-sm"
+                    v-model="user.lastname"
+                    :rules="[required('Lastname')]"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row align="center" justify="center" class="mb-3">
+                <v-col cols="12" md="3">
+                  <v-select
+                    :items="['Male', 'Female']"
+                    label="Gender"
+                    outlined
+                    v-model="user.gender"
+                    class="rounded-sm"
+                    :disabled="loading"
+                    :rules="[required('Gender')]"
+                  ></v-select>
+                </v-col>
+                <v-col cols="12" md="3">
+                  <v-menu
+                    ref="menu"
+                    v-model="menu"
+                    :close-on-content-click="false"
+                    transition="scale-transition"
+                    offset-y
+                    min-width="290px"
+                  >
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-text-field
+                        v-model="user.birthday"
+                        label="Birth Date"
+                        readonly
+                        outlined
+                        class="rounded-sm"
+                        v-bind="attrs"
+                        v-on="on"
+                        :disabled="loading"
+                        :rules="[required('Birth date')]"
+                      ></v-text-field>
+                    </template>
+                    <v-date-picker
+                      ref="picker"
                       v-model="user.birthday"
-                      label="Birth Date"
-                      readonly
-                      outlined
-                      class="rounded-sm"
-                      v-bind="attrs"
-                      v-on="on"
-                    ></v-text-field>
-                  </template>
-                  <v-date-picker
-                    ref="picker"
-                    v-model="user.birthday"
-                    :max="new Date().toISOString().substr(0, 10)"
-                    min="1950-01-01"
-                    @change="dateSave"
-                  ></v-date-picker>
-                </v-menu>
-              </v-col>
-              <v-col cols="12" md="3">
-                <v-select
-                  :items="['Single', 'Married', 'Divorced', 'Widowed']"
-                  label="Civil Status"
-                  outlined
-                  class="rounded-sm"
-                  value="Single"
-                ></v-select>
-              </v-col>
-            </v-row>
-          </template>
-        </user-details> <!-- USER PERSONAL DETAILS INFO -->
+                      :max="new Date().toISOString().substr(0, 10)"
+                      min="1950-01-01"
+                      @change="dateSave"
+                    ></v-date-picker>
+                  </v-menu>
+                </v-col>
+                <v-col cols="12" md="3">
+                  <v-select
+                    :items="['Single', 'Married', 'Divorced', 'Widowed']"
+                    label="Civil Status"
+                    outlined
+                    class="rounded-sm"
+                    value="Single"
+                    :disabled="loading"
+                    :rules="[required('Civil Status')]"
+                  ></v-select>
+                </v-col>
+              </v-row>
+            </template>
+          </user-details> <!-- USER PERSONAL DETAILS -->
 
+          <user-details :users="users">
+            <template #user-title>
+              <div class="gray--text text-uppercase font-weight-bold">
+                Contact Details
+              </div>
+            </template>
+            <template #user-text-fields="{ user }">
+              <v-row align="center" justify="center">
+                <v-col cols="12" md="3">
+                  <v-text-field
+                    label="Contact Number"
+                    outlined
+                    class="rounded-sm"
+                    v-model="user.contact_number"
+                    :rules="[required('Contact Number')]"
+                    :disabled="loading"
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" md="3">
+                  <v-text-field
+                    label="Occupation"
+                    outlined
+                    class="rounded-sm"
+                    v-model="user.occupation"
+                    :rules="[required('Occupation')]"
+                    :disabled="loading"
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" md="3">
+                  <v-select
+                    :items="['ADMIN', 'OPM', 'PRODUCTION', 'QUALITY CONTROL', 'TECHNICAL', 'SECURITY']"
+                    label="Department"
+                    outlined
+                    class="rounded-sm"
+                    v-model="user.department"
+                    :rules="[required('Department')]"
+                    :disabled="loading"
+                  ></v-select>
+                </v-col>
+              </v-row>
+              <v-row align="center" justify="center">
+                <v-col cols="12" md="3">
+                  <v-select
+                    :items="['SPMI', 'VACI', 'GGC', 'SECURITY']"
+                    label="Company"
+                    outlined
+                    class="rounded-sm"
+                    v-model="user.company"
+                    :rules="[required('Company')]"
+                    :disabled="loading"
+                  ></v-select>
+                </v-col>
+                <v-col cols="12" md="3">
+                  <v-select
+                    :items="countries"
+                    label="Nationality"
+                    outlined
+                    class="rounded-sm"
+                    v-model="user.nationality"
+                    :rules="[required('Nationality')]"
+                    :disabled="loading"
+                  ></v-select>
+                </v-col>
+                <v-col cols="12" md="3">
+                  <v-text-field
+                    label="Address"
+                    class="rounded-sm"
+                    v-model="user.address"
+                    :rules="[required('Address')]"
+                    outlined
+                    :disabled="loading"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+            </template>
+          </user-details> <!-- USER CONTACT DETAILS -->
+        </v-form>
 
-        <user-details :users="users">
-          <template #user-title>
-            <div class="gray--text text-uppercase font-weight-bold">
-              Contact Details
-            </div>
-          </template>
-          <template #user-text-fields="{ user }">
-            <v-row align="center" justify="center">
-              <v-col cols="12" md="3">
-                <v-text-field
-                  label="Contact Number"
-                  outlined
-                  class="rounded-sm"
-                  v-model="user.contact_number"
-                ></v-text-field>
-              </v-col>
-              <v-col cols="12" md="3">
-                <v-text-field
-                  label="Occupation"
-                  outlined
-                  class="rounded-sm"
-                  v-model="user.occupation"
-                ></v-text-field>
-              </v-col>
-              <v-col cols="12" md="3">
-                <v-select
-                  :items="['ADMIN', 'OPM', 'PRODUCTION', 'QUALITY CONTROL', 'TECHNICAL', 'SECURITY']"
-                  label="Department"
-                  outlined
-                  class="rounded-sm"
-                  v-model="user.department"
-                ></v-select>
-              </v-col>
-            </v-row>
-            <v-row align="center" justify="center">
-              <v-col cols="12" md="3">
-                <v-select
-                  :items="['SPMI', 'VACI', 'GGC', 'SECURITY']"
-                  label="Company"
-                  outlined
-                  class="rounded-sm"
-                  v-model="user.company"
-                ></v-select>
-              </v-col>
-              <v-col cols="12" md="3">
-                <v-select
-                  :items="countries"
-                  label="Nationality"
-                  outlined
-                  class="rounded-sm"
-                  v-model="user.nationality"
-                ></v-select>
-              </v-col>
-              <v-col cols="12" md="3">
-                <v-text-field
-                  label="Address"
-                  class="rounded-sm"
-                  v-model="user.address"
-                  outlined
-                ></v-text-field>
-              </v-col>
-            </v-row>
-          </template>
-        </user-details>
         
         <v-row align="center" justify="center" class="mb-3">
-          <v-btn class="primary text-capitalize mr-2" depressed>
+          <v-btn class="primary text-capitalize mr-2" 
+                 depressed
+                 :loading="loading"
+                 @click="onSaveAccountChanges">
             <v-icon left>mdi-content-save</v-icon> Save Changes
           </v-btn>
-          <v-btn class="text-capitalize gray--text" outlined>
+          <v-btn class="text-capitalize gray--text" 
+                 outlined
+                 :disabled="loading">
             <v-icon left>mdi-close</v-icon> Clear Fields
           </v-btn>
         </v-row>
-        
       </v-col>
     </v-row>
   </div>
@@ -208,9 +237,13 @@
     },
     data () {
       return {
+        loading: false,
         menu: false,
         users: [],
-        countries: []
+        countries: [],
+        required (propertyType) { 
+          return v => v && v.length > 0 || `${propertyType} is required.`
+        }
       }
     },
     methods: {
@@ -221,6 +254,13 @@
       },
       dateSave (date) {
         this.$refs.menu.save(date)
+      },
+
+      // ==== ON SAVE DATA CHANGES ===
+      onSaveAccountChanges () {
+        if (this.$refs.form.validate()) {
+          this.loading = true
+        }
       }
     },
     created () {
