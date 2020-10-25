4d112d5b545cc8caa347f6e06c100280ce5609bf
acatzk <joshuaimalay@gmail.com+>
2020-10-25 08:44:24 +0800

    Import UserDetails and reused in user-fields

--- src/components/pages/home-account/Index.vue
+++ src/components/pages/home-account/Index.vue
@@ -13,93 +13,95 @@
     <v-row align="center" justify="center">
       <v-col cols="12">
         
-        <user-personal-details>
-          <template #info-title>
+        <user-details>
+          <template #user-title>
             <div class="gray--text text-uppercase font-weight-bold">
               Personal Details
             </div>
           </template>
-          <template #user-fullname>
-            <v-col cols="12" md="3">
-              <v-text-field
-                label="Firstname"
-                outlined
-                class="rounded-sm"
-                value="Joshua"
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12" md="3">
-              <v-text-field
-                label="Middlename"
-                outlined
-                class="rounded-sm"
-                value="Adoro"
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12" md="3">
-              <v-text-field
-                label="Lastname"
-                outlined
-                class="rounded-sm"
-                value="Galit"
-              ></v-text-field>
-            </v-col>
+          <template #user-text-fields>
+            <v-row align="center" justify="center" class="mb-3">
+              <v-col cols="12" md="3">
+                <v-text-field
+                  label="Firstname"
+                  outlined
+                  class="rounded-sm"
+                  value="Joshua"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" md="3">
+                <v-text-field
+                  label="Middlename"
+                  outlined
+                  class="rounded-sm"
+                  value="Adoro"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" md="3">
+                <v-text-field
+                  label="Lastname"
+                  outlined
+                  class="rounded-sm"
+                  value="Galit"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+            <v-row align="center" justify="center" class="mb-3">
+              <v-col cols="12" md="3">
+                <v-select
+                  :items="['Male', 'Female']"
+                  label="Gender"
+                  outlined
+                  value="Male"
+                  class="rounded-sm"
+                ></v-select>
+              </v-col>
+              <v-col cols="12" md="3">
+                <v-menu
+                  ref="menu"
+                  transition="scale-transition"
+                  offset-y
+                  min-width="290px"
+                >
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-text-field
+                      label="Birth Date"
+                      readonly
+                      outlined
+                      class="rounded-sm"
+                      v-bind="attrs"
+                      v-on="on"
+                      value="March 20, 1999"
+                    ></v-text-field>
+                  </template>
+                  <v-date-picker
+                    ref="picker"
+                    :max="new Date().toISOString().substr(0, 10)"
+                    min="1950-01-01"
+                  ></v-date-picker>
+                </v-menu>
+              </v-col>
+              <v-col cols="12" md="3">
+                <v-select
+                  :items="['Single', 'Married', 'Divorced', 'Widowed']"
+                  label="Civil Status"
+                  outlined
+                  class="rounded-sm"
+                  value="Single"
+                ></v-select>
+              </v-col>
+            </v-row>
           </template>
-          <template #user-details>
-            <v-col cols="12" md="3">
-              <v-select
-                :items="['Male', 'Female']"
-                label="Gender"
-                outlined
-                value="Male"
-                class="rounded-sm"
-              ></v-select>
-            </v-col>
-            <v-col cols="12" md="3">
-              <v-menu
-                ref="menu"
-                transition="scale-transition"
-                offset-y
-                min-width="290px"
-              >
-                <template v-slot:activator="{ on, attrs }">
-                  <v-text-field
-                    label="Birth Date"
-                    readonly
-                    outlined
-                    class="rounded-sm"
-                    v-bind="attrs"
-                    v-on="on"
-                    value="March 20, 1999"
-                  ></v-text-field>
-                </template>
-                <v-date-picker
-                  ref="picker"
-                  :max="new Date().toISOString().substr(0, 10)"
-                  min="1950-01-01"
-                ></v-date-picker>
-              </v-menu>
-            </v-col>
-            <v-col cols="12" md="3">
-              <v-select
-                :items="['Single', 'Married', 'Divorced', 'Widowed']"
-                label="Civil Status"
-                outlined
-                class="rounded-sm"
-                value="Single"
-              ></v-select>
-            </v-col>
-          </template>
-        </user-personal-details> <!-- USER PERSONAL DETAILS INFO -->
+        </user-details> <!-- USER PERSONAL DETAILS INFO -->
 
 
-        <user-contact-details>
-          <template #info-details>
+        <user-details>
+          <template #user-title>
             <div class="gray--text text-uppercase font-weight-bold">
               Contact Details
             </div>
           </template>
-          <template #user-contact>
+          <template #user-text-fields>
             <v-row align="center" justify="center">
               <v-col cols="12" md="3">
                 <v-text-field
@@ -156,7 +158,7 @@
               </v-col>
             </v-row>
           </template>
-        </user-contact-details>
+        </user-details>
         
       </v-col>
     </v-row>
@@ -167,9 +169,8 @@
   import countries from '@/static/countries.json'
   export default {
     components: {
-      UserAuthProfile: () => import('./UserAuthProfile'),
-      UserPersonalDetails: () => import('./UserPersonalDetails'),
-      UserContactDetails: () => import('./UserContactDetails')
+      UserDetails: () => import('./UserDetails'),
+      UserAuthProfile: () => import('./UserAuthProfile')
     },
     data () {
       return {
