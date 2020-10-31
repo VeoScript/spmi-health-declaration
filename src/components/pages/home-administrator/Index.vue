7a523986e67c0b35e462480f2d94e1a8f51edbaa
acatzk <joshuaimalay@gmail.com+>
2020-11-01 07:20:56 +0800

    Moved the file inside general-results folder

--- src/components/pages/home-administrator/Index.vue
+++ src/components/pages/home-administrator/Index.vue
@@ -23,7 +23,7 @@
 <script>
   export default {
     components: {
-      StatisticDialog: () => import('./StatisticDialog')
+      StatisticDialog: () => import('./general-results/StatisticDialog')
     },
     data () {
       return {
