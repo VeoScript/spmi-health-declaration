3d3b3412ee7fa3c5ffba7686308fd0d71300ef60
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2020-11-07 14:40:15 +0800

    update typography

--- src/views/TermsAndCondition.vue
+++ src/views/TermsAndCondition.vue
@@ -6,17 +6,19 @@
         <v-row>
           <v-col sm="12" md="12" cols="12">
             <v-card flat>
-              <div>
+              <div class="mt-5 font-weight-medium subtitle-1">
                 I hereby authorize SPMI, to collect and process
-                the data indicated herein for the purpose of effective control <br>
-                of the COVID-19 infection. I understand that my personal information is protected by RA 10173, <br>
-                Data Privacy Act of 2012, and that I am required by <br>
+                the data indicated herein for the purpose of effective control
+                of the COVID-19 infection. I understand that my personal information is protected by RA 10173,
+                Data Privacy Act of 2012, and that I am required by
                 RA 11469, Bayanihan to Heal as One Act, to provide truthful information. <br><br>
-                This system is only for the SPMI Employees and the employees under agencies. <br>
-                Any kinds of false information registered herein the program can cause a crime under the cyber law. <br>
-                ©{{ new Date().getFullYear() }} Covid-19 Health Declaration, Specialty Pulp Manufacturing Inc. All rights reserved.
+                This system is only for the SPMI Employees and the employees under agencies.
+                Any kinds of false information registered herein the program can cause a crime under the cyber law.
               </div>
               <!-- <small>Designed and Developed by Team veoscript & acatzk.</small> -->
+              <div class="mt-5 font-weight-light subtitle-2">
+                ©{{ new Date().getFullYear() }} Covid-19 Health Declaration, Specialty Pulp Manufacturing Inc. All rights reserved.
+              </div>
             </v-card>
           </v-col>
         </v-row>
