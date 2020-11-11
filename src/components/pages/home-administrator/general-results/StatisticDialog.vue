890a62e8d0530b5c8c10050e32274cf41f4a0b7d
Joshua Galit <acatzk>
2020-11-11 16:31:43 +0800

    ========= Statistic Report for month and Year work success and can revert any time ===============

--- src/components/pages/home-administrator/general-results/StatisticDialog.vue
+++ src/components/pages/home-administrator/general-results/StatisticDialog.vue
@@ -5,6 +5,67 @@
         <h2 class="font-weight-medium display">
           <v-icon left large>mdi-clipboard-text</v-icon> Statistic Report
         </h2>
+        <v-row class="mt-5">
+          <v-spacer></v-spacer>
+          <v-form ref="form" 
+                  class="d-flex justify-center align-items-center mx-2"
+                  :disabled="loading">
+            <v-menu ref="menu"
+                    v-model="menu"
+                    :close-on-content-click="false"
+                    transition="scale-transition"
+                    offset-y
+                    min-width="290px">
+              <template v-slot:activator="{ on, attrs }">
+                <v-text-field v-model="date_entry"
+                              label="Date Entry"
+                              readonly
+                              v-bind="attrs"
+                              dense
+                              outlined
+                              :rules="[required('Date of Entry')]"
+                              v-on="on">
+                </v-text-field>
+              </template>
+              <v-date-picker ref="picker"
+                            v-model="date_entry"
+                            min="2019-01-01"
+                            @change="dateEntrySave">
+              </v-date-picker>
+            </v-menu>
+            <v-menu ref="menu2"
+                    v-model="menu2"
+                    :close-on-content-click="false"
+                    transition="scale-transition"
+                    offset-y
+                    min-width="290px">
+              <template v-slot:activator="{ on, attrs }">
+                <v-text-field v-model="date_end"
+                              label="Date End"
+                              readonly
+                              outlined
+                              dense
+                              class="ml-1"
+                              v-bind="attrs"
+                              v-on="on"
+                              :rules="[required('Date End')]">
+                </v-text-field>
+              </template>
+              <v-date-picker ref="picker"
+                            v-model="date_end"
+                            min="2019-01-01"
+                            @change="dateEndSave">
+              </v-date-picker>
+            </v-menu>
+            <v-btn depressed
+                   color="primary"
+                   @click="onSearchRecord"
+                   class="ml-1"
+                   :loading="loading">
+                  <v-icon left>mdi-magnify</v-icon> Search
+            </v-btn>
+          </v-form>
+        </v-row>
       </v-card-title>
       <v-container>
         <v-row class="d-flex justify-center">
@@ -23,7 +84,7 @@
                 </v-card-title>
               </template>
               <template #total-result>
-                2000
+                {{ overAll ? overAll : 0 }}
               </template>
             </statistic-result>
           </v-col>
@@ -42,7 +103,7 @@
                 </v-card-title>
               </template>
               <template #total-result>
-                2000
+                {{ accepted ? accepted : 0 }}
               </template>
             </statistic-result>
           </v-col>
@@ -61,7 +122,7 @@
                 </v-card-title>
               </template>
               <template #total-result>
-                2000
+                {{ denied ? denied : 0 }}
               </template> 
             </statistic-result>
           </v-col>
@@ -72,6 +133,8 @@
 </template>
 
 <script>
+  import gql from 'graphql-tag'
+  import toastAlertStatus from '@/utils'
   export default {
     components: {
       StatisticResult: () => import('./StatisticResult')
@@ -80,6 +143,21 @@
       visible: {
         type: Boolean,
         required: true
+      }
+    },
+    data () {
+      return {
+        date_entry: '',
+        date_end: '',
+        menu: false,
+        menu2: false,
+        overAll: 0,
+        accepted: 0,
+        denied: 0,
+        loading: false,
+        required (propertyType) { 
+          return v => v && v.length > 0 || `${propertyType} is required.`
+        }
       }
     },
     computed: {
@@ -92,6 +170,100 @@
             this.$emit('close')
           }
         }
+      }
+    },
+    methods: {
+      dateEntrySave (date) {
+        this.$refs.menu.save(date)
+      },
+      dateEndSave (date) {
+        this.$refs.menu2.save(date)
+      },
+      onSearchRecord () {
+        if (this.$refs.form.validate()) {
+          this.overAllReport()
+          this.totalAcceptedReport()
+          this.totalDenied()
+        }
+      },
+      overAllReport () {
+        this.loading = true
+        this.$apollo.query({
+          query: gql`
+            query overAllResult($date_entry: timestamptz, $date_end: timestamptz) {
+              checklist_result_aggregate(where: {created_at: {_gte: $date_entry}, _and: {created_at: {_lte: $date_end}}}) {
+                aggregate {
+                  count
+                }
+              }
+            }
+          `,
+          variables: {
+            date_entry: this.date_entry,
+            date_end: this.date_end
+          }
+        })
+        .then(({ data }) => {
+          this.loading = false
+          this.overAll = data.checklist_result_aggregate.aggregate.count
+          // console.log(data.checklist_result_aggregate.aggregate.count)
+        })
+        .catch(error => {
+          this.loading = false
+          toastAlertStatus('error', error)
+        })
+      },
+      totalAcceptedReport () {
+        this.loading = true
+        this.$apollo.query({
+          query: gql`
+            query totalAcceptedReport($date_entry: timestamptz, $date_end: timestamptz) {
+              checklist_result_aggregate(where: {created_at: {_gte: $date_entry}, _and: {created_at: {_lte: $date_end}, _and: {isAccepted: {_eq: true}}}}) {
+                aggregate {
+                  count
+                }
+              }
+            }
+          `,
+          variables: {
+            date_entry: this.date_entry,
+            date_end: this.date_end
+          }
+        })
+        .then(({ data }) => {
+          this.loading = false
+          this.accepted = data.checklist_result_aggregate.aggregate.count
+        })
+        .catch(error => {
+          this.loading = false
+          toastAlertStatus('error', error)
+        })
+      },
+      totalDenied () {
+        this.loading = true
+        this.$apollo.query({
+          query: gql`
+            query totalAcceptedReport($date_entry: timestamptz, $date_end: timestamptz) {
+              checklist_result_aggregate(where: {created_at: {_gte: $date_entry}, _and: {created_at: {_lte: $date_end}, _and: {isAccepted: {_eq: false}}}}) {
+                aggregate {
+                  count
+                }
+              }
+            }
+          `,
+          variables: {
+            date_entry: this.date_entry,
+            date_end: this.date_end
+          }
+        })
+        .then(({ data }) => {
+          this.loading = false
+          this.denied = data.checklist_result_aggregate.aggregate.count
+        })
+        .catch(error => {
+          this.loading = false
+          toastAlertStatus('error', error)
+        })
       }
     }
   }
